9d4f735f89e712a0ff57f2d0041046b7c34e861d
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-06-05 08:12:06 -0600

    2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void numerosParesImpares(int num) {
-    cout << "Numeros pares e impares hasta " << num << ":\n";
+    cout << "Numeros pares e impares hasta " << num << endl;
     for (int i = 1; i <= num; i++) {
         if (i % 2 == 0) {
             cout << i << " es un numero par" << endl;
